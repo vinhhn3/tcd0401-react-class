49132f308412f1b0a5ba23bf747ed22fe37b8741
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-06-09 11:11:17 +0700

    Add Users component with static data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 
 class App extends Component {
   // Properties
@@ -22,11 +22,7 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        <UserItem />
-        <UserItem />
-        <UserItem />
-        <UserItem />
-        <UserItem />
+        <Users />
       </div>
     );
   }
