826075f826999e29bf91e6dfd4a392ed1fcbe53d
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-06-09 10:34:51 +0700

    State and JSX introduction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Properties
+  state = {
+    fullName: "Vinh Hoang",
+    address: "Le Loi",
+    counter: 0,
+    isDisplay: false,
+  };
+
+  // Methods
+  increaseCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        {this.state.isDisplay && <p>{this.state.fullName}</p>}
+        <p>{this.state.address}</p>
+        <p>{this.state.counter}</p>
+        <button onClick={this.increaseCounter}>Increase Counter</button>
+      </div>
+    );
+  }
 }
 
 export default App;
